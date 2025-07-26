c39c5a97f3e0605c352b59d51d344911190d54e5
Linus <linus.breitenberger@gmail.com>
2025-07-26 19:32:03 +0200

    pull all pokemon

--- config.py
+++ config.py
@@ -13,11 +13,24 @@ REQUEST_DELAY = 0.1  # 100ms between requests
 # Gen I: Bulbasaur to Mew
 # Gen II: Chikorita to Celebi
 # Gen III: Treecko to Deoxys
+# Gen IV: Turtwig to Arceus
+# Gen V: Victini to Genesect
+# Gen VI: Chespin to Volcanion
+# Gen VII: Rowlet to Marshadow (plus Meltan/Melmetal optional)
+# Gen VIII: Grookey to Enamorus
+# Gen IX: Sprigatito to Pecharunt (as of July 2025)
 GEN_LIMITS = {
-    1: (1, 15),
-    2: (152, 161),
-    3: (252, 266)
+    1: (1, 151),
+    2: (152, 251),
+    3: (252, 386),
+    4: (387, 493),
+    5: (494, 649),
+    6: (650, 721),
+    7: (722, 809),
+    8: (810, 905),
+    9: (906, 1025)  # Pecharunt is currently the last known Pokémon
 }
+
 
 # --- Logging Configuration ---
 def setup_logging():
